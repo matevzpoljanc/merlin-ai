ddad5d8c1bb9b3e86ec993aca769048a88a201de
Miha Grcar <miha@sellestial.com>
2024-01-12 12:15:38 +0100

    Updates decorators

--- merlin_ai/__init__.py
+++ merlin_ai/__init__.py
@@ -1,5 +1,5 @@
-from .decorators import ai_enum, ai_model
+from .decorators import ai_enum, ai_model, ai_enum_ex
 from .ai_classes import BaseAIClass
 from .settings import default_model_settings
 
-__all__ = ["ai_model", "ai_enum", "default_model_settings", "BaseAIClass"]
+__all__ = ["ai_model", "ai_enum", "ai_enum_ex", "default_model_settings", "BaseAIClass"]
